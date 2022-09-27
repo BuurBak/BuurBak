6595effca64dbcabde4d86627d3717f3cc38a29b
spark-156 <lucabergman@hotmail.nl>
2022-09-27 18:07:50 +0200

    Email token creation and confirmation

---
+++ api/src/main/java/com/buurbak/api/security/service/EmailConfirmationTokenService.java
@@ -0,0 +1,37 @@
+package com.buurbak.api.security.service;
+
+import com.buurbak.api.security.model.EmailConfirmationToken;
+import com.buurbak.api.security.model.User;
+import com.buurbak.api.security.repository.EmailConfirmationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class EmailConfirmationTokenService {
+    private final EmailConfirmationTokenRepository emailConfirmationTokenRepository;
+    public static final int EMAIL_TOKEN_DURATION = 24 * 60 * 60; // 24 hours in seconds
+    public EmailConfirmationToken createAndSaveEmailConfirmationToken (User user) {
+        EmailConfirmationToken token = new EmailConfirmationToken(
+                LocalDateTime.now(),
+                LocalDateTime.now().plusSeconds(EMAIL_TOKEN_DURATION),
+                user
+        );
+        emailConfirmationTokenRepository.save(token);
+        return token;
+    }
+
+    public EmailConfirmationToken findById(UUID tokenId) throws EntityNotFoundException {
+        return emailConfirmationTokenRepository.findById(tokenId).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Transactional
+    public void setConfirmedAtToNow(UUID tokenId) {
+        emailConfirmationTokenRepository.updateConfirmedAt(tokenId, LocalDateTime.now());
+    }
+}
