2128ad8456eecd6c3d85c78a53b2a969020a1dc8
spark-156 <lucabergman@hotmail.nl>
2022-10-05 11:34:38 +0200

    Refactor UserController to customer controller

---
+++ api/src/main/java/com/buurbak/api/users/controller/CustomerController.java
@@ -0,0 +1,44 @@
+package com.buurbak.api.users.controller;
+
+import com.buurbak.api.users.controller.dto.CustomerDTO;
+import com.buurbak.api.users.model.Customer;
+import com.buurbak.api.users.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping("self")
+    @Operation(summary = "Get self")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
+    public CustomerDTO getCustomerSelf(Authentication authentication) {
+        try {
+            Customer customer = customerService.findByUsername(authentication.getName());
+            return new CustomerDTO(customer.getId(), customer.getEmail(), customer.getName(), customer.getDateOfBirth(), customer.getIban(), customer.getAddress());
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (AccessDeniedException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+    }
+}
--- api/src/main/java/com/buurbak/api/users/repository/CustomerRepository.java
+++ api/src/main/java/com/buurbak/api/users/repository/CustomerRepository.java
@@ -1,9 +1,13 @@
 package com.buurbak.api.users.repository;
 
-import com.buurbak.api.security.repository.UserRepository;
 import com.buurbak.api.users.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface CustomerRepository extends UserRepository<Customer> {
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findByEmail(String email);
 }
