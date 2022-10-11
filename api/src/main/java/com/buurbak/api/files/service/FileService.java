4524603b337d7f64d7650e7b4870ddc905d8b688
spark-156 <lucabergman@hotmail.nl>
2022-10-11 11:30:20 +0200

    YAGNI principle

--- api/src/main/java/com/buurbak/api/files/service/FileService.java
+++ api/src/main/java/com/buurbak/api/files/service/FileService.java
@@ -4,12 +4,8 @@ import com.buurbak.api.files.model.FileEntity;
 import com.buurbak.api.files.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
-import java.io.IOException;
-import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -17,25 +13,7 @@ import java.util.UUID;
 public class FileService {
     private final FileRepository fileRepository;
 
-    public void save(MultipartFile file) throws IOException {
-        try {
-            FileEntity fileEntity = new FileEntity();
-            fileEntity.setName(StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename())));
-            fileEntity.setContentType(file.getContentType());
-            fileEntity.setData(file.getBytes());
-            fileEntity.setSize(file.getSize());
-
-            fileRepository.save(fileEntity);
-        } catch (IOException exception) {
-            throw new IOException("Could not save file: " + file.getOriginalFilename(), exception);
-        }
-    }
-
     public FileEntity findById(UUID id) throws EntityNotFoundException {
         return fileRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
-
-//    public List<FileEntity> getAllFiles() {
-//        return fileRepository.findAll();
-//    }
 }
