5f5d8b9a304182b96af5706b05724221ef21de9d
spark-156 <lucabergman@hotmail.nl>
2022-10-13 11:29:29 +0200

    optimize imports

--- api/src/test/java/com/buurbak/api/files/service/FileServiceTest.java
+++ api/src/test/java/com/buurbak/api/files/service/FileServiceTest.java
@@ -1,6 +1,5 @@
 package com.buurbak.api.files.service;
 
-import com.buurbak.api.files.model.FileEntity;
 import com.buurbak.api.files.repository.FileRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +11,6 @@ import javax.persistence.EntityNotFoundException;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
 
 class FileServiceTest {
 
