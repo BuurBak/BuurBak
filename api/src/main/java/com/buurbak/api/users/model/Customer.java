77f16ce22477fd644edca603ee13255fbc009673
spark-156 <lucabergman@hotmail.nl>
2022-10-24 10:37:21 +0200

    Add constructor for Customer with roles

--- api/src/main/java/com/buurbak/api/users/model/Customer.java
+++ api/src/main/java/com/buurbak/api/users/model/Customer.java
@@ -1,18 +1,18 @@
 package com.buurbak.api.users.model;
 
+import com.buurbak.api.security.model.Role;
 import com.buurbak.api.security.model.User;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.time.LocalDate;
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 public class Customer extends User {
     @Column(columnDefinition = "text")
     private String name;
@@ -26,6 +26,14 @@ public class Customer extends User {
 
     public Customer(String email, String password, String name, LocalDate dateOfBirth, String iban, String address) {
         super(email, password);
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.iban = iban;
+        this.address = address;
+    }
+
+    public Customer(String email, String password, Collection<Role> roles, String name, LocalDate dateOfBirth, String iban, String address) {
+        super(email, password, roles);
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.iban = iban;
