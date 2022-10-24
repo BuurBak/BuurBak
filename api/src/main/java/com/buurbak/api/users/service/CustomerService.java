75ca55f86a0fa0ea9db73ee98b5fa59c3f8b6a5a
spark-156 <lucabergman@hotmail.nl>
2022-10-24 15:33:26 +0200

    update service with specification and pages

--- api/src/main/java/com/buurbak/api/users/service/CustomerService.java
+++ api/src/main/java/com/buurbak/api/users/service/CustomerService.java
@@ -1,18 +1,32 @@
 package com.buurbak.api.users.service;
 
+import com.buurbak.api.users.exception.CustomerNotFoundException;
 import com.buurbak.api.users.model.Customer;
 import com.buurbak.api.users.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 
 @Service
 @AllArgsConstructor
+@Transactional
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
     public Customer findByUsername(String name) throws EntityNotFoundException {
-        return customerRepository.findByEmail(name).orElseThrow(EntityNotFoundException::new);
+        return customerRepository.findByEmail(name).orElseThrow(CustomerNotFoundException::new);
+    }
+
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    public Page<Customer> findAll(Specification<Customer> specification, Pageable pageable) {
+        return customerRepository.findAll(specification, pageable);
     }
 }
