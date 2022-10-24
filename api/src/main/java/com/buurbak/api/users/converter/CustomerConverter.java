e5bb02b13913e0bec1ac797bf478b7813d0edfb7
spark-156 <lucabergman@hotmail.nl>
2022-10-24 15:31:43 +0200

    add CustomerConverter

--- api/src/main/java/com/buurbak/api/users/converter/CustomerConverter.java
+++ api/src/main/java/com/buurbak/api/users/converter/CustomerConverter.java
@@ -4,7 +4,11 @@ import com.buurbak.api.users.dto.PrivateCustomerDTO;
 import com.buurbak.api.users.dto.PublicCustomerDTO;
 import com.buurbak.api.users.model.Customer;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CustomerConverter {
@@ -16,5 +20,13 @@ public class CustomerConverter {
     public PrivateCustomerDTO convertCustomerToPrivateCustomerDTO (Customer customer) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(customer, PrivateCustomerDTO.class);
+    }
+
+    public Page<PublicCustomerDTO> convertCustomerPageToPublicCustomerDTOPage (Page<Customer> customerPage) {
+        List<PublicCustomerDTO> publicCustomerDTOList = customerPage
+                .stream()
+                .map(this::convertCustomerToPublicCustomerDTO)
+                .toList();
+        return new PageImpl<>(publicCustomerDTOList, customerPage.getPageable(), customerPage.getTotalElements());
     }
 }
