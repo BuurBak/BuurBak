1d1691a514f2e9053d0de654092999d3d5c0d285
Koen <koen.baltus@gmail.com>
2022-11-14 16:02:49 +0100

    WIP unit tests traileroffer

--- api/src/test/java/com/buurbak/api/trailers/service/TrailerOfferServiceTest.java
+++ api/src/test/java/com/buurbak/api/trailers/service/TrailerOfferServiceTest.java
@@ -3,19 +3,25 @@ package com.buurbak.api.trailers.service;
 import com.buurbak.api.trailers.model.TrailerOffer;
 import com.buurbak.api.trailers.repository.TrailerOfferRepository;
 import org.jeasy.random.EasyRandom;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,7 +52,7 @@ class TrailerOfferServiceTest {
 
     @Test
     void trailerOfferDoesNotExistsById(){
-        //Given
+        // Given
         UUID uuid = UUID.randomUUID();
 
         // When
@@ -56,22 +62,51 @@ class TrailerOfferServiceTest {
     }
 
     @Test
-    @Disabled
     void trailerOffersDoesNotExists() {
-        //given
+        // Given
+        List<TrailerOffer> trailerOfferList = new ArrayList<>();
+        // When
+        when(trailerOfferService.getAllTrailerOffersInfo(any())).thenReturn(new PageImpl<>(trailerOfferList));
 
-        //when
+        Page<TrailerOffer> result = trailerOfferService.getAllTrailerOffersInfo(PageRequest.of(0, 20));
 
-        //then
+        // Then
+
+        assertTrue(result.getContent().isEmpty());
     }
 
     @Test
-    @Disabled
     void trailerOffersExists() {
         //given
+        EasyRandom easyRandom = new EasyRandom();
+        List<TrailerOffer> trailerOfferList = new ArrayList<>();
+        for(int i = 0; i < 20; i++){
+            trailerOfferList.add(easyRandom.nextObject(TrailerOffer.class));
+        }
 
         //when
+        when(trailerOfferService.getAllTrailerOffersInfo(any())).thenReturn(new PageImpl<>(trailerOfferList));
+
+        Page<TrailerOffer> result = trailerOfferService.getAllTrailerOffersInfo(PageRequest.of(0, 20));
 
         //then
+        assertEquals(trailerOfferList, result.getContent());
+    }
+
+    @Test
+    void trailerOfferPagesExists() {
+        //given
+        EasyRandom easyRandom = new EasyRandom();
+        List<TrailerOffer> trailerOfferList = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            trailerOfferList.add(easyRandom.nextObject(TrailerOffer.class));
+        }
+
+        //when
+        when(trailerOfferService.getAllTrailerOffersInfo(any())).thenReturn(new PageImpl<>(trailerOfferList));
+
+        Page<TrailerOffer> result = trailerOfferService.getAllTrailerOffersInfo(Pageable.ofSize(20));
+        //then
+        assertEquals(5, result.getTotalPages());
     }
 }
