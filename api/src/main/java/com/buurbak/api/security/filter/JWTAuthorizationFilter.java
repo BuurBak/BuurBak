197b497583fe4e91e580e16c7ffa5cf0b5db021a
spark-156 <lucabergman@hotmail.nl>
2022-11-16 14:28:18 +0100

    fix securityUtils = null error

--- api/src/main/java/com/buurbak/api/security/filter/JWTAuthorizationFilter.java
+++ api/src/main/java/com/buurbak/api/security/filter/JWTAuthorizationFilter.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.buurbak.api.security.util.SecurityUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,6 +28,7 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Autowired
     private final SecurityUtils securityUtils = new SecurityUtils();
 
     @Override
--- api/src/main/java/com/buurbak/api/security/service/TokenService.java
+++ api/src/main/java/com/buurbak/api/security/service/TokenService.java
@@ -2,6 +2,8 @@ package com.buurbak.api.security.service;
 
 import com.auth0.jwt.JWT;
 import com.buurbak.api.security.util.SecurityUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,8 +11,10 @@ import java.util.Date;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class TokenService {
-    private final SecurityUtils securityUtils = new SecurityUtils();
+    @Autowired
+    private SecurityUtils securityUtils;
 
     public static final int ACCESS_TOKEN_DURATION = 8 * 60 * 60 * 1000;
     public static final int REFRESH_TOKEN_DURATION = ACCESS_TOKEN_DURATION * 3 * 6;
