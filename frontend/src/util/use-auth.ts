863d45b6041fda924b7a614e173de08166053add
spark-156 <lucabergman@hotmail.nl>
2022-12-08 12:10:14 +0100

    fix weird merge

--- frontend/src/util/use-auth.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { authContext } from './auth-context'
-
-// Hook for child components to get the auth object ...
-// ... and re-render when it changes.
-export const useAuth = () => {
-  return useContext(authContext)
-}
