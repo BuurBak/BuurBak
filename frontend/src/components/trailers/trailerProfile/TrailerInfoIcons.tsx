ac4f91939d8ab8ceb579cb119e6921c384bea354
spark-156 <lucabergman@hotmail.nl>
2022-12-21 12:08:47 +0100

    Update carrying capacity icon

--- frontend/src/components/trailers/trailerProfile/TrailerInfoIcons.tsx
+++ frontend/src/components/trailers/trailerProfile/TrailerInfoIcons.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@mui/material'
 import TrailerInfoIcon from './TrailerInfoIcon'
-import { TbArrowsLeftRight } from 'react-icons/tb'
 import React from 'react'
 import { TrailerOffer } from '../../../types/TrailerOffer'
+import { GiWeight } from 'react-icons/gi'
 
 export default function TrailerInfoIcons({
   trailer,
@@ -14,7 +14,7 @@ export default function TrailerInfoIcons({
     <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
       <Box gridColumn={gridColumn}>
         <TrailerInfoIcon
-          icon={<TbArrowsLeftRight size={40} />}
+          icon={<GiWeight size={40} />}
           subHeader={`${trailer.capacity} kg`}
           title="Draagcapaciteit"
         />
